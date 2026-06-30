9e7be1babbeb8361e2485ffce5327c11d373b94f
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-06-30 17:06:09 +0200

    feat(sparkle): publish Netfox v0.11.0 to appcast

--- config/index.ts
+++ config/index.ts
@@ -86,7 +86,7 @@ export default {
     defaultLanguage: 'en',
   },
   app: {
-    version: '0.10.0',
+    version: '0.11.0',
     minMacOS: '15.0',
     downloadUrl: 'https://github.com/gfazioli/netfox-website/releases/latest',
   },
